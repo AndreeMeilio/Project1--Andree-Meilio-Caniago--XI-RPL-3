c311ad1dc2787503ba9618a871d9ade80b074d62
Meilio Andree <meilioAndree27@gmail.com>
2021-01-27 08:49:28 +0700

    initial commit

---
+++ Aplikasi Kasir Restoran/src/Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+    }
+}
--- Aplikasi Kasir Restoran/src/me/andreemeilio/aplikassi_kasir/Main.java
+++
@@ -1,7 +0,0 @@
-package me.andreemeilio.aplikassi_kasir;
-
-public class Main {
-    public static void main(String[] args) {
-        
-    }
-}
