0d687e4e9efae1ae66438dfc9b336767f5ff4bf4
Meilio Andree <meilioAndree27@gmail.com>
2021-02-17 15:16:31 +0700

    Implementasi Kelas Daftar Menu

--- src/me/andreemeilio/classes/DaftarMenu.java
+++ src/me/andreemeilio/classes/DaftarMenu.java
@@ -6,19 +6,30 @@ public class DaftarMenu {
     private ArrayList<Menu> daftarMenu;
 
     public DaftarMenu(){
-
+        daftarMenu = new ArrayList<>();
     }
 
     
     public void tambahMenu(Menu menu){
-
+        daftarMenu.add(menu);
     }
 
     public void getMenuByKategori(String kategori){
+        System.out.println("======== " + kategori + " ========");
 
+        for (int i = 0; i < daftarMenu.size(); i++){
+            Menu m = daftarMenu.get(i);
+            if (m.getKategori().equals(kategori)){
+                System.out.println((i + 1) + ". " + m.getNama_menu() + "\t" + m.getHarga());
+            }
+        }
     }
 
     public void tampilDaftarMenu(){
-        
+        System.out.println("======== ALDEBARAMEN ========");
+        getMenuByKategori("Ramen");
+        getMenuByKategori("Kuah");
+        getMenuByKategori("Toping");
+        getMenuByKategori("Minuman");
     }
 }
